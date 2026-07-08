5b3085d738b442a3523cc23b3cc5379391a3a9a3
YuvalN1211 <yuvaln2010@gmail.com>
2026-07-08 16:38:34 +0300

    finish + comments

--- Client.py
+++ Client.py
@@ -1,5 +1,10 @@
 import socket
 import os
+import math
+import sys
+
+
+print("Client\n-----------")
 
 def get_socket_info():
     your_device = str(input("Are you trying to comunicate with a server that is on your device (y/n)"))
@@ -14,16 +19,18 @@ def get_socket_info():
     info = (ip, port)
     return info
 
+# make socket
 client_socket = socket.socket()
 info = get_socket_info()
 client_socket.connect(info)
 
+# upload function 2
 def send_file_size(path):
     file_size = os.stat(path).st_size
-    encoded_file_size = str(file_size).encode()
-    print(f"{file_size} - {encoded_file_size}")
-    client_socket.send(encoded_file_size)
+    byte_data = file_size.to_bytes(4, byteorder='big')
+    client_socket.send(byte_data)
 
+# upload function 1
 def send_file_to_server(path):
     send_file_size(path)
 
@@ -33,32 +40,57 @@ def send_file_to_server(path):
         client_socket.send(data)
         data = f.read(1024)
 
-def download_file_from_server(file_name):
-    pass
+
+# download function
+def download_file_from_server(path):
+    file = open(path, "wb")
+    recive_bytes = client_socket.recv(4)
+    file_size = int.from_bytes(recive_bytes, byteorder='big')
+    
+    print(f"file size is {file_size} bytes")
+    i = math.ceil(file_size / 1024)
+        
+    while i > 0:
+        chunk = client_socket.recv(1024)
+        file.write(chunk)
+        i -= 1
+    print("finished writing")
 
 
-
+# function for sending the name for the server, upload and download
 def send_file_name(string):
-    file_name = str(input(f"Enter the file name {string} (including type, like: my_file.txt)"))
+    file_name = str(input(f"Enter the file name {string} (including type, like: my_file.txt): "))
+    length = str(len(file_name))
+    print(f"length of name: {length}")
+    client_socket.send(length.encode())
     client_socket.send(file_name.encode())
     return file_name
 
-def user_function():
-    q = str(input("Do you want to upload or download a file? (u/d)"))
+def main():
+    q = str(input("Do you want to upload or download a file? (u/d/bye): "))
     if q == "u":
         client_socket.send(q.encode())
         send_file_name("to upload")
 
         file_path = str(input("Enter file path to a file you want to upload: "))
         send_file_to_server(file_path)
+        main()
 
     elif q == "d":
         client_socket.send(q.encode())
         file_name = send_file_name("to download")
         
-        download_file_from_server(file_name)
+        file_path = str(input("Enter path to a directory you want to download to: "))
+        path = f"{file_path}\{file_name}"
+        download_file_from_server(path)
+        main()
+    elif q == "bye":
+        client_socket.send(q.encode())
+        sys.exit()
+
     else:
-        print("Expected (u/d)")
+        print("Expected (u/d/bye)")
+        main()
 
 
-user_function()
+main()
--- Server.py
+++ Server.py
@@ -1,8 +1,25 @@
 import socket
+import os
 import math
 
+print("Server\n-----------")
+
+# my computer or not for server storage directory
+def Yuval_check():
+    Yuval = str(input("Is this Yuval's computer? (y/n)"))
+    if Yuval == "y":
+        return r"C:\Users\yuval\OneDrive\אקדמיית_המתכנתים\רשתות_תקשורת_ופרוטוקולים\ClientServerSysForUploadingFiles\ServerStorage"
+    elif Yuval == "n":
+        base_dir = str(input("Enter path for a directory that will be the server storage: "))
+        return fr"{base_dir}"
+    else:
+        print("error, expected (y/n)")
+        Yuval_check()
+
+base_dir = Yuval_check()
+
 def get_socket_info():
-    your_device = str(input("Are you trying to comunicate with a client that is on your device (y/n)"))
+    your_device = str(input("Are you trying to comunicate with a client that is on your device (y/n): "))
     if your_device == "y":
         ip = "127.0.0.1"
     elif your_device == "n":
@@ -14,6 +31,7 @@ def get_socket_info():
     info = (ip, port)
     return info
 
+# make socket
 server_socket = socket.socket()
 info = get_socket_info()
 print(info)
@@ -23,27 +41,60 @@ print("Waiting for connections")
 connected_socket, address = server_socket.accept()
 print("Connection from: " + str(address))
 
+# upload function
 def recive_data(path):
     file = open(path, "wb")
-    file_size = int(connected_socket.recv(4).decode())
+    recive_bytes = connected_socket.recv(4)
+    file_size = int.from_bytes(recive_bytes, byteorder='big')
     
     print(f"file size is {file_size} bytes")
     i = math.ceil(file_size / 1024)
         
     while i > 0:
-        chunk = socket.recv(1024)
-        print(f"\nWriting {chunk}\n")
+        chunk = connected_socket.recv(1024)
         file.write(chunk)
         i -= 1
+    print("finished writing")
 
-def send_data():
-    pass
+# download function 2
+def send_file_size(path):
+    file_size = os.stat(path).st_size
+    byte_data = file_size.to_bytes(4, byteorder='big')
+    connected_socket.send(byte_data)
 
-q = connected_socket.recv(1).decode()
-if q == "u":
-    # recive_data(r"C:\Users\yuval\OneDrive\אקדמיית_המתכנתים\רשתות_תקשורת_ופרוטוקולים\ClientServerSysForUploadingFiles\ServerStorage.txt")
-    file_name = connected_socket.recv(1)
-    recive_data(rf"C:\Users\yuval\OneDrive\אקדמיית_המתכנתים\רשתות_תקשורת_ופרוטוקולים\ClientServerSysForUploadingFiles\ClientData\{file_name}")
-elif q == "d":
-    print("download chosen")
-    send_data()
+# download function 1
+def send_data(path):
+    send_file_size(path)
+
+    f = open(path, "rb")
+    
+    data = f.read(1024)
+    while data:
+        connected_socket.send(data)
+        data = f.read(1024)
+
+# main help function
+def get_file_name():
+    bytes = int(connected_socket.recv(1).decode())
+    file_name = connected_socket.recv(bytes).decode()
+    return file_name
+
+
+def main():
+    global base_dir
+    while True:
+        q = connected_socket.recv(1).decode()
+        file_name = get_file_name()
+        path = f"{base_dir}\{file_name}"
+
+        if q == "u":
+            print(f"path of file to upload {path}")
+            recive_data(path)
+        elif q == "d":
+            send_data(path)
+        elif q == "bye":
+            connected_socket.close()
+            server_socket.close()
+            break
+
+main()
